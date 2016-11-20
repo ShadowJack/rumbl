04dddc9fb7e6d6b9ebb5e95dee8b7addb2b2ac39
shadowjack <kostyap@hotmail.ru>
2016-11-20 23:47:48 +0300

    Add videos, categories and some tests

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,36 +2,4 @@ defmodule Rumbl.UserControllerTest do
   use Rumbl.ConnCase
   alias Rumbl.{Repo, User}
 
-  setup do
-    { :ok, user } = Repo.insert(%User{name: "Kostya", username: "ShadowJack", password_hash: "123"})
-    Repo.insert(%User{name: "Olya", username: "Sheeffer", password_hash: "password"})
-    { :ok, user_id: user.id }
-  end
-
-  test "GET /users", %{conn: conn} do
-    conn = get(conn, "/users")
-    
-    assert html_response(conn, 200) =~ "Kostya"
-    assert html_response(conn, 200) =~ "Olya"
-  end
-
-  test "GET /users/:id", %{conn: conn, user_id: user_id} do
-    conn = get(conn, "/users/#{user_id}")
-
-    assert html_response(conn, 200) =~ "Kostya"
-    assert html_response(conn, 200) =~ "ShadowJack"
-  end
-
-  test "GET /users/new", %{conn: conn} do
-    conn = get(conn, "/users/new")
-
-    assert html_response(conn, 200) =~ "Create User"
-    assert html_response(conn, 200) =~ "User"
-    assert html_response(conn, 200) =~ "Username"
-    assert html_response(conn, 200) =~ "Password"
-  end
-
-  test "POST /users", %{conn: conn} do
-    "Not implemented"
-  end
 end
--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -6,6 +6,8 @@ defmodule Rumbl.Auth do
 
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Phoenix.Controller
+  alias Rumbl.Router.Helpers
 
   @doc """
   Saves logged in user in session
@@ -39,4 +41,20 @@ defmodule Rumbl.Auth do
   def logout(conn) do
     configure_session(conn, drop: true)
   end
+  
+  @doc """
+  Checks if user is logged in and
+  if not it forbids access to the resource
+  """
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
+  end
+
 end
